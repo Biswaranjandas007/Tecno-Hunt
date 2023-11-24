e1c2530cd6b1d92167e0ed104744c5f32a4f33d1
Biswaranjandas007 <biswaranjandasb33@gmail.com>
2023-11-24 14:36:19 +0530

    frist commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+window.onscroll =function(){
+    if(document.body.scrollTop > 0 || document.documentElement.scrollTop > 0){
+        document.getElementById('navbar').classList.add('scrolled')
+    }
+    else{
+        document.getElementById('navbar').classList.remove('scrolled')
+    }
+}
+
+//ads
+// AOS
+AOS.init({
+    duration: 800,
+});
